d526682ed7a7732a827ccf5dec84397923822bbb
Mateus Teixeira <mateusbpt1@gmail.com>
2018-05-15 17:01:44 -0300

    Ajustes nas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { UsersComponent } from './components/users/users.component';
 import { ChallengesComponent } from './components/challenges/challenges.component';
 import { FeedbackComponent } from './components/feedback/feedback.component';
+import { GroupsComponent } from './components/groups/groups.component';
+import { FriendsComponent } from './components/friends/friends.component';
 
 const routes: Routes = [
   { path: '', component: ProfileComponent },
   { path: 'users', component: UsersComponent },
   { path: 'challenges', component: ChallengesComponent },
   { path: 'feedback', component: FeedbackComponent },
-  { path: 'friends', component: ChallengesComponent },
-  { path: 'groups', component: FeedbackComponent }
+  { path: 'friends', component: FriendsComponent },
+  { path: 'groups', component: GroupsComponent }
 ];
 
 @NgModule({
