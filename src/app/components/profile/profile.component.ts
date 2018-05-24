8ffd6a411b0d0726529a5ca1410cc05edb2d2546
Mateus Teixeira <mateusbpt1@gmail.com>
2018-05-23 21:51:41 -0300

    Adicionando postagens

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-profile',
@@ -7,11 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  closeResult: string;
 
-  public cardLength : number = 1;
+  constructor(private modalService: NgbModal) { }
 
-  public user : any = { name: "Mateus", lastname : "Teixeira" };
+  public cardLength: number = 1;
+
+  public user: any = { name: "Mateus", lastname: "Teixeira" };
+
+  open(content) {
+    this.modalService.open(content, { centered: true, size: 'lg' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 
   ngOnInit() {
   }
